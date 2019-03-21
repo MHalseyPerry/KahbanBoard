b04f5ee5498a589c7874cae20172f5938c37fb4d
Milo Halsey-Perry <milo.halseyperry@gmail.com>
2019-03-21 17:12:01 +0000

    Added comments to JS.

--- scripts/main.js
+++ scripts/main.js
@@ -1,39 +1,36 @@
 (function(){
-
-    let counter = 0;
-    let addButtons = [...document.getElementsByClassName('add-button')];
-    addButtons.forEach(btn => btn.addEventListener('click', addTask));
+    let addButtons = [...document.getElementsByClassName('add-button')];   // Assign button elements to variable.
+    addButtons.forEach(btn => btn.addEventListener('click', addTask));   // Add eventlistener to all addbuttons, click calls addTask.
 
     const containers = {
         todo: document.getElementById('todo'),
-        inProgress: document.getElementById('in-progress'),
+        inProgress: document.getElementById('in-progress'),   // Create "containers" object containing content elements of each column.
         complete: document.getElementById('complete'),
     };
 
-    const tasks = getInitialTasks();
+    const tasks = getInitialTasks();   // Load previously tasks from local storage.
 
-    render();
+    render();   // display loaded tasks.
 
     function addTask (e) {
-        const task = prompt('Please enter the task name');
-        const type = e.target.dataset.type;
-        tasks[type].push(task);
+        const task = prompt('Please enter the task name');   // Save user input to "task" variable
+        const type = e.target.dataset.type;   // Saves target button to variable "type".
+        tasks[type].push(task);   // Push new task to tasks array
 
-        render();
-        store();
+        render();   // Diplay tasks & new task.
+        store();   // Call store function.
     }
 
-    function render () {
+    function render () {   // Function to refresh view, deletes all task elements and recreates them including any new additions.
 
         for (const [key, container] of Object.entries(containers)) {
-            while (container.lastChild) {
+            while (container.lastChild) {   // While child(task) exists remove child(task).
                 container.removeChild(container.lastChild);
             }
 
             tasks[key].forEach(task => {
-                const el = document.createElement('div');
+                const el = document.createElement('div'); // Create elements from contents of task object.
                 el.className = 'item';
-
                 el.innerHTML = task;
                 container.appendChild(el);
             });
@@ -41,13 +38,14 @@
     }
 
     function store () {
-        localStorage.setItem('tasks', JSON.stringify(tasks));
+        localStorage.setItem('tasks', JSON.stringify(tasks)); // Add stringifyed tasks object to storage
     }
+
 
     function getInitialTasks () {
         let stringified = localStorage.getItem('tasks');
 
-        if (!stringified) {
+        if (!stringified) {     // if no tasks in storage return empty object
             return {
                 todo: [],
                 inProgress: [],
